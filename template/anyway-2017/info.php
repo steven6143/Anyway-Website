26ee50a733f797a07ec2a4fb99056a379050f0e3
JJ Ying <yingjunjiu@gmail.com>
2017-01-29 17:01:06 +0800

    New Template

---
+++ template/anyway-2017/info.php
@@ -0,0 +1,10 @@
+<?PHP
+$template['id']="anyway-2017";
+$template['name']="anyway-2017";
+$template['author']="JJ Ying";
+$template['intro']="Designed by JJ.Ying     http://IconMoon.com";
+$template['dirname']="anyway-2017";
+$template['thumbnail']="thumb.jpg";
+$template['structure']="template/anyway-2017/elements.php";
+$template['images']="template/anyway-2017/images";
+$template['css'][0]="template/anyway-2017/styles.css";
