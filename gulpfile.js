e5ed7ff028ce3909b1524b5f8ed85324f303123d
JJ Ying <yingjunjiu@gmail.com>
2017-04-28 16:43:48 +0800

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    plugins = gulpLoadPlugins();
+ 
+var cdnUrl = [
+	[ 'css/main.css', 'http://anyway-web.b0.upaiyun.com/anyway.fm/main.min.css' ]
+];
+ 
+gulp.task('watch', function() {
+	gulp.watch(['*','*/*'], ['default']);
+ });
+ 
+ 
+gulp.task('default', function() {
+
+	gulp.src('template/anyway/elements.php')
+		.pipe(plugins.batchReplace(cdnUrl))
+		.pipe(gulp.dest('build'));
+
+	gulp.src('css/*.css')
+		.pipe(plugins.cleanCss({compatibility: 'ie8'}))
+		.pipe(plugins.rename({
+		      suffix: '.min'
+		    }))
+		.pipe(gulp.dest('build'));     
+});
+
