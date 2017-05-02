5db3afde853215ac9c557ef8e99461030486419d
JJ Ying <yingjunjiu@gmail.com>
2017-05-02 16:10:15 +0800

    Testing disqus

--- template/anyway-2017/elements.php
+++ template/anyway-2017/elements.php
@@ -333,7 +333,26 @@ $elements['viewentry']=<<<eot
 
 
 
-<script type="text/javascript" src="/anyway/hashover/hashover.js"></script>
+<!--  <script type="text/javascript" src="/anyway/hashover/hashover.js"></script> -->
+
+<div id="disqus_thread"></div>
+<script>
+
+var disqus_config = function () {
+this.page.url = 'http://Anyway.FM'
+this.page.identifier = 'aabbccdd'
+};
+
+
+(function() {
+var d = document, s = d.createElement('script');
+s.src = 'https://anyway-fm.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                                
 
 
 	</div>
